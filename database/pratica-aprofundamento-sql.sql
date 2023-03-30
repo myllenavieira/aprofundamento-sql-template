7d830e7f0bd0748729b593a370c32cababfef584
myllenavieira <vieirasilvamyllena@gmail.com>
2023-03-29 21:23:34 -0300

    Exercícios

--- database/pratica-aprofundamento-sql.sql
+++ database/pratica-aprofundamento-sql.sql
@@ -1,3 +1,4 @@
+-- Active: 1680127027189@@127.0.0.1@3306
 -- Conecte o arquivo pratica-aprofundamento-sql.db com a extensão MySQL e ative a conexão aqui
 
 -- Deletar tabela
@@ -43,3 +44,54 @@ VALUES
 SELECT * FROM pokemons;
 
 -- Práticas
+
+-- Prática 1 --
+
+-- Buscar (SELECT) pokemons com speed maior (>) que 60 --
+SELECT * FROM pokemons
+WHERE speed > 60;
+
+-- Buscar (SELECT) pokemons que tenham attack e special_attack maiores ou iguais (>=) a 60 --
+SELECT * FROM pokemons
+WHERE attack >= 60 AND special_attack >= 60;
+
+-- Buscar (SELECT) pokemons que tenham no nome o termo "saur" no final
+SELECT * FROM pokemons
+WHERE name LIKE "%saur";
+
+-- Buscar pokemon que tenha "to" no meio do nome --
+SELECT * FROM pokemons
+WHERE name LIKE "%to%";
+
+-- Buscar pokemons que onome começa com "Ch" --
+SELECT * FROM pokemons
+WHERE name LIKE "ch%";
+
+-- Prática 2 --
+
+-- Busque na tabela pokemons a média simples (AVG) da coluna hp --
+SELECT AVG(hp) AS MédiaHP FROM pokemons;
+
+-- Busque o número (COUNT) do número de linhas da tabela pokemons --
+SELECT COUNT(*) AS TotalDePokemons FROM pokemons;
+
+-- Prática 3 --
+-- Busque todos os pokemons e ordene-os baseado na coluna defense em ordem decrescente (DESC) --
+SELECT * FROM pokemons
+ORDER BY defense DESC;
+
+-- Busque novamente o número de pokemons cadastrados baseado na coluna type --
+SELECT COUNT(*) AS NumeroDeTipos, type FROM pokemons
+GROUP BY "type";
+
+-- Busque todos os pokemons, limite o resultado a 3 linhas iniciando a partir da 5 linha --
+SELECT * FROM pokemons
+LIMIT 3
+OFFSET 5;
+
+-- Fixação --
+-- Busque todos os pokemons, filtre para mostrar somente os tipos "fire" ou "grass", ordem crescente na coluna attack, limitado a 3 linhas iniciando a partir da terceira --
+SELECT * FROM pokemons
+WHERE "type" = "fire" OR "type" = "grass"
+ORDER BY attack ASC
+LIMIT 3 OFFSET 2;
